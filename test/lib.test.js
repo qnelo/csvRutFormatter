b3255d6ab91fc5e64b27e2567374b13caeadf3ff
Camilo Henriquez <camilo.henriquez.mena@gmail.com>
2017-01-11 17:33:28 -0300

    formateo de ruts

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,12 +1,29 @@
 import test from 'ava';
-import TestClass from '../lib/test-class.js';
+const rut = require('../lib/rut-class.js');
 
-let testInstance = new TestClass('a');
+let ruts = ['156491373', '9457904k'];
+let rutTest = new rut(ruts);
 
-test('Test 1', t => {
-    t.pass();
+test('Return rut formatter', t => {
+    t.is(rutTest.set('156491373'), '15.649.137-3');
 });
 
-test('Return must be equal to `a-b`', t => {
-    t.is(testInstance.testMethod('b'), 'a-b');
+test('Return rut formatter with `-`', t => {
+    t.is(rutTest.set('15649137-3'), '15.649.137-3');
+});
+
+test('Return rut formatter with `-` & `.`', t => {
+    t.is(rutTest.set('15.649.137-3'), '15.649.137-3');
+});
+
+test('Return rut formatter with `---` & `...`', t => {
+    t.is(rutTest.set('1.5.6.4.9.1.3-7-3'), '15.649.137-3');
+});
+
+test('Return array of formatted ruts position 0', t => {
+    t.is(rutTest.formattedArray()[0], '15.649.137-3');
+});
+
+test('Return array of formatted ruts position 1', t => {
+    t.is(rutTest.formattedArray()[1], '9.457.904-K');
 });
