3b629121d83acd0843c6d24c0a3fbba71cfe78c1
Camilo Henriquez <camilo.henriquez.mena@gmail.com>
2017-01-16 19:08:02 -0300

    adding proveedores ruts.csv to test

--- test/csv.test.js
+++ test/csv.test.js
@@ -7,6 +7,7 @@ test.cb('Return instanceof Array & compare 0, 350400 positions', t => {
     // Arrange & Act
     csv.getArray((data) => {
         // Assert
+        // console.log(JSON.stringify(data, null, 4));
         t.is(data[0], '50.534.620-3');
         t.is(data[350400], '76.365.580-6');
         t.is(data instanceof Array, true);
